4618a11d36dcf62573d50bba0848bf2b8009561f
Mieraisan <mieraisan@gmail.com>
2022-12-12 15:48:58 +0000

    Create pyram.sh

---
+++ pyram.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Set the maximum number of rows
+max_rows=6
+
+# Loop through the rows, starting from 1
+for ((i=1; i<=max_rows; i++)); do
+
+  # Print the leading spaces
+  for ((j=i; j<max_rows; j++)); do
+    printf " "
+  done
+
+  # Print the numbers in the row
+  for ((j=1; j<=i; j++)); do
+    printf "%d" $j
+  done
+
+  # Print a newline at the end of each row
+  printf "\n"
+done
